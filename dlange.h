5960764874c8637a9349a62f61b678cf4414a617
Simon Haendeler <a1268476@univis.ac.at>
2017-01-10 20:58:50 +0100

    refactor and arguments

---
+++ dlange.h
@@ -0,0 +1,7 @@
+extern double dlange_ ( const char * const norm,
+		const int * const m,
+		const int * const n,
+		const double * A,
+		const int * const lda,
+              	double * const work
+		);
--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <quark.h>
 #include <papi.h>
 #include <cblas.h>
+#include "dlange.h"
 
 int is_close(double a, double b){
 	return (fabs(a - b) < 0.1E-12);
@@ -31,7 +32,7 @@ void generateMatrix(double *A, int n) {
 
 }
 
-	
+
 void printMatrix(const double * A, int n) {
 
 	printf("\n\n%s\n", "Matrix: ");
@@ -75,44 +76,64 @@ double residual(double* L, double* A, double* work, int n) {
 
 }
 
-int main()
-{
-	int n = 1000;
-	double* A = calloc(n*n, sizeof(double));
-	double* B = calloc(n*n, sizeof(double));
-	PLASMA_enum uplo=PlasmaUpper; //here: does not matter, as we store the full matrix
-	generateMatrix(A, n);
-	cloneMatrix(A, B, n);
-	int error;
+int measure_PLASMA_dpotrf(double* A, double* B, double* work, PLASMA_enum uplo, int n, int cores) {
 
 	// papi variables
 	float rtime, ptime, mflops;
 	long long flpops;
+
 	double res;
+	int error;
 
-	error = PLASMA_Init(1); //spawn cores for PLASMA
+	error = PLASMA_Init(cores);
 	if (error != PLASMA_SUCCESS) {
-		printf("Error in PLASMA_Init: %i\n", error);
-		exit (1);
+		printf("{\"type\": \"error\", \"errorcode\": %i, \"msg\": \"Error in PLASMA_Init: %i\"}\n", error, error);
+		exit(1);
 	}
-
-	//printMatrix(A, n);
 
 	PAPI_flops(&rtime, &ptime, &flpops, &mflops);
 	error = PLASMA_dpotrf(uplo, n, A, n);
 	PAPI_flops(&rtime, &ptime, &flpops, &mflops);
 
-	double * work = malloc(n*n*sizeof(double));
+
 	res = residual(A, B, work, n);
-	printf("{ \"n\": %i, \"rtime\" : %f, \"ptime\": %f, \"flpops\": %llu , \"mflops\": %f, \"res\": %f}\n", n, rtime, ptime, flpops, mflops, res);
+
+	printf("{ \"type\":\"result\", \"data\":{ \"n\": %i, \"rtime\" : %f, \"ptime\": %f, \"flpops\": %llu , \"mflops\": %f, \"res\": %f, \"cores\": %i}}\n", n, rtime, ptime, flpops, mflops, res, cores);
 
 	if (error != PLASMA_SUCCESS) {
-		printf("Error in PLASMA_dpotrf: %i\n", error);
-		exit (1);
+		printf("{\"type\": \"error\", \"errorcode\": %i, \"msg\": \"Error in PLASMA_dpotrf: %i\"}\n", error, error);
+		exit(1);
 	}
 	error = PLASMA_Finalize();
 	if (error != PLASMA_SUCCESS) {
-		printf("Error in PLASMA_Finalize:%i\n", error);
-		exit (1);
+		printf("{\"type\": \"error\", \"errorcode\": %i, \"msg\": \"Error in PLASMA_Finalize:%i\"}\n", error, error);
+		exit(1);
 	}
+
+	return 0;
+}
+
+/**
+ *
+ * main N CORES
+ *
+ * N (int) - dimension of problem
+ * CORES (int) - how many cores to use for plasma
+ *
+ */
+int main(int argc, char **argv)
+{
+	if (argc < 2) {
+		printf("Too few arguments\n");
+		exit(1);
+	}
+	int n = atoi(argv[1]);
+	int cores = atoi(argv[2]);
+	double* A = calloc(n*n, sizeof(double));
+	double* B = calloc(n*n, sizeof(double));
+	double * work = malloc(n*n*sizeof(double));
+	PLASMA_enum uplo=PlasmaUpper; //here: does not matter, as we store the full matrix
+	generateMatrix(A, n);
+	cloneMatrix(A, B, n);
+	measure_PLASMA_dpotrf(A, B, work, uplo, n, cores);
 }
